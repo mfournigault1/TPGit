4a2721cb95674e304c9ba278fa0d30f64388b8b9
mfournigault <mfournigault@btslivh.eu>
2021-01-07 15:48:36 +0100

    on repare

--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: snir2g1
- *
- * Created on 5 janvier 2021, 17:48
- */
 #include "iostream"
 #include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-    cout << "Bonjour, fichier Arno" << endl;
+    cout << "Bonjour" << endl;
+    cout << "Hello" << endl;
+    cout << "je comprend pas" << endl;
     return 0;
 }
-
