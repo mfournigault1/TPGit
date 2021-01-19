e2c1243f05a66172769b675229e09178d89fd543
abiard <abiard@btslivh.eu>
2021-01-19 16:46:49 +0100

    arno modifs

--- main_recup.cpp
+++ main_recup.cpp
@@ -9,6 +9,5 @@ int main(int argc, char** argv) {
     cout << "Bonjour" << endl;
     cout << "Hello" << endl;
     cout << "premier message de matthéo" << endl;
-    cout << "réponse de Arno ^^ " << endl;
     return 0;
 }
